d8998b968c6c5577b19f4f04431b317ffbb1b6f4
umesshk <umeshkumar01439@gmail.com>
2026-07-10 12:17:25 +0530

    writng to docs

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-title = input("Enter the title : ")
-shorts_link = input("Enter Shorts Link: ")
-original_link = input("Enter Original Link : ")
-
-
-print(title, shorts_link, original_link)
-=======
 from auth import authenticate
 from docs import append_entry, get_docs_services
 from dotenv import load_dotenv
@@ -27,4 +19,3 @@ original_url = input("Original Url: ")
 shorts_url = input("Shorts Url : ")
 
 append_entry(service, DOCUMENT_ID, number, title, shorts_url, original_url)
->>>>>>> 1c27f46 (writng to docs)
